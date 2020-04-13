a57fcb557db6ba18871cf2dd4ef5adfbbba1e5a3
MarcosSmeets <48696729+MarcosSmeets@users.noreply.github.com>
2020-04-13 19:31:56 -0300

    Add files via upload

---
+++ jarvis project/jarvis1.py
@@ -0,0 +1,54 @@
+from gtts import gTTS
+import speech_recognition as sr
+import os
+import webbrowser
+import smtplib
+
+def talkToMe(audio):
+    print(audio)
+    tts = gTTS(text = audio, lang='en')
+    tts.save('audio.mp3')
+    os.system('sapi5 audio.mp3')
+
+#listem for commands
+
+def myCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('I am ready for your next command')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration = 1)
+        audio = r.listem(source)
+
+    try:
+        command = r.recognize_google(audio)
+        print('you said: ' + command + '/n')
+    
+    #loop back to listen to commands
+
+    except sr.UnknowValeuError:
+        assistant(myCommand())
+    
+    return command
+
+#if statements for executing commands
+
+def assistant(command):
+    if 'open Reddit python' in command:
+        chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+        url = 'https://www.reddit.com/r/python'
+        webbrowser.get(chrome_path).open(url)
+    if 'what\'s up' in command:
+        talkToMe('Chillin bro')
+    if 'drop my needle' in command:
+        chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+        url = "https://open.spotify.com"
+        webbrowser.get(chrome_path).open(url)
+
+        
+    
+    
+talkToMe('I am ready for your new command')
+
+while True:
+    assistant(myCommand())
